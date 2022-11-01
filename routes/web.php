5713ada372bdfb2bb99545ebb50807434fcdac15
xu qian su <s3a932003@student.ncut.edu.tw>
2022-11-01 18:37:14 +0800

    串接 Controller & 設定 Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home.index');
-Route::get('/about', function () {
-    return view('welcome');
-})->name('about.index');
-Route::get('/news', function () {
-    return view('welcome');
-})->name('news.index');
+Route::get('/', [HomeController::class, 'index'] )->name('home.index');
+Route::get('/about',[AboutController::class, 'index'] )->name('about.index');
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');
